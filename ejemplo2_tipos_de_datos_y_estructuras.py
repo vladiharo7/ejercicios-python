43986ac45f693a6bb39c04e9cafe0f50b282113b
Edward Haro <vladiharo7@hotmail.com>
2025-10-11 19:07:58 -0500

    Add ejemplo2

---
+++ ejemplo2_tipos_de_datos_y_estructuras.py
@@ -0,0 +1,19 @@
+# Ejercicio tipos de datos en Python
+
+# 1. Variable de tipo entero (int) para almacenar la edad
+age = 25
+
+# 2. Variable de tipo flotante (float) para almacenar la altura en metros
+height = 1.75
+
+# 3. Variable de tipo booleano (bool) para indicar si soy estudiante o no
+is_student = True
+
+# 4. Variable de tipo cadena de texto (str) para almacenar el nombre
+name = "Edward Haro"
+
+# Imprimimos cada variable en una línea separada
+print(age)
+print(height)
+print(is_student)
+print(name)
